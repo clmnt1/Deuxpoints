d14d22c21f1cb88c1b850f3f6d87ef945f7416fb
clmnt1 <clemtkd@hotmail.com>
2013-09-24 23:01:19 +0200

    [FIX] - Format products data and load it on view

--- application/controller/CategoryController.php
+++ application/controller/CategoryController.php
@@ -4,7 +4,8 @@ class CategoryController extends Controller
 {
 
 	public function process(){
-
+		$this->view->nbProduct = $this->product->getNbProduct();
+		$this->view->productData = $this->product->getProductData();
 	}
 
 }
--- application/model/Product.php
+++ application/model/Product.php
@@ -4,6 +4,7 @@ class Product
 {
 	
 	private $productData = array();
+	private $nbProduct;
 	
 	public function __construct(Connect $connect){
 		$this->connect = $connect;
@@ -14,16 +15,21 @@ class Product
 		$this->id = $id;
 	}
 	
-	public function getProductData(){
-		$req = $this->connect->query('SELECT * FROM produits WHERE idProduit =' . $this->id);
-		$productData = mysqli_fetch_array($req);
-		return $productData;
-	}
-	
-	public function nbProduct(){
+	public function getNbProduct(){
 		$req = $this->connect->query('SELECT COUNT(idproduit) AS nbProduit FROM produits');
 		$nbProduct = mysqli_fetch_array($req);
 		return $nbProduct;
 	}
 
+	public function getProductData(){
+		$req = $this->connect->query('SELECT * FROM produits');
+		$productData = array();
+		$productData = mysqli_fetch_all($req);
+		/*while($productData = mysqli_fetch_assoc($req)){
+			$productData[] = $productData;
+			var_dump($productData);
+		}*/
+		return $productData;
+	}
+	
 }
